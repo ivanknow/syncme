00d2de675c3d555e32cff4efb5aa090a103a20d3
ivanknow@gmail.com <ivanknow@gmail.com@2da3ea3a-f62f-6fb5-de89-798bcc474610>
2014-07-29 20:28:41 +0000

    Modificação da forma de conexão para uma forma mais atualizada que permite setar propriedades como timeout para proximas versões

--- trunk/android2/src/br/net/ifrs/syncme/JSONParser.java
+++ trunk/android2/src/br/net/ifrs/syncme/JSONParser.java
@@ -1,24 +1,21 @@
 package br.net.ifrs.syncme;
 
 import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.List;
- 
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
+
 import org.apache.http.NameValuePair;
 import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.utils.URLEncodedUtils;
-import org.apache.http.impl.client.DefaultHttpClient;
 import org.json.JSONException;
 import org.json.JSONObject;
- 
+
 import android.util.Log;
  
 public class JSONParser {
@@ -42,26 +39,12 @@ public class JSONParser {
  
             // check for request method
             if(method == "POST"){
-                // request method is POST
-                // defaultHttpClient
-                DefaultHttpClient httpClient = new DefaultHttpClient();
-                HttpPost httpPost = new HttpPost(url);
-                httpPost.setEntity(new UrlEncodedFormEntity(params));
- 
-                HttpResponse httpResponse = httpClient.execute(httpPost);
-                HttpEntity httpEntity = httpResponse.getEntity();
-                is = httpEntity.getContent();
- 
+            	json = sendPost(url,params);
+            	 Log.e("JSON",json);
             }else if(method == "GET"){
-                // request method is GET
-                DefaultHttpClient httpClient = new DefaultHttpClient();
-                String paramString = URLEncodedUtils.format(params, "utf-8");
-                url += "?" + paramString;
-                HttpGet httpGet = new HttpGet(url);
- 
-                HttpResponse httpResponse = httpClient.execute(httpGet);
-                HttpEntity httpEntity = httpResponse.getEntity();
-                is = httpEntity.getContent();
+             
+                json = sendGet(url,params);
+                Log.e("JSON",json);
             }          
  
         } catch (UnsupportedEncodingException e) {
@@ -70,32 +53,88 @@ public class JSONParser {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
-        }
- 
-        try {
-            BufferedReader reader = new BufferedReader(new InputStreamReader(
-                    is, "iso-8859-1"), 8);
-            StringBuilder sb = new StringBuilder();
-            String line = null;
-            while ((line = reader.readLine()) != null) {
-                sb.append(line + "\n");
-            }
-            is.close();
-            json = sb.toString();
         } catch (Exception e) {
-            Log.e("Buffer Error", "Error converting result " + e.toString());
-        }
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
  
         // try parse the string to a JSON object
         try {
             jObj = new JSONObject(json);
         } catch (JSONException e) {
-            Log.e("JSON Parser", "Error parsing data " + e.toString());
+            Log.e("JSON Parser", "Rolas"+json);
         }
  
         // return JSON String
         return jObj;
  
     }
+    
+ // HTTP GET request
+ 	private String sendGet(String url,
+            List<NameValuePair> params) throws Exception {
+
+        String paramString = URLEncodedUtils.format(params, "utf-8");
+        url += "?" + paramString;
+        
+ 		URL obj = new URL(url);
+ 		HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+  
+ 		// optional default is GET
+ 		con.setRequestMethod("GET");
+ 		
+ 		int responseCode = con.getResponseCode();
+  
+ 		BufferedReader in = new BufferedReader(
+ 		        new InputStreamReader(con.getInputStream(),"iso-8859-1"),8);
+ 		String inputLine;
+ 		StringBuffer response = new StringBuffer();
+  
+ 		while ((inputLine = in.readLine()) != null) {
+ 			response.append(inputLine + "\n");
+ 		}
+ 		in.close();
+  
+ 		//print result
+ 		return response.toString();
+  
+ 	}
+  
+ 	// HTTP POST request
+ 	private String sendPost(String url,
+            List<NameValuePair> params) throws Exception {
+  
+ 		URL obj = new URL(url);
+ 		HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+  
+ 		//add reuqest header
+ 		con.setRequestMethod("POST");
+ 		con.setRequestProperty("Accept-Language", "en-US,en;q=0.5");
+  
+ 		String urlParameters =  URLEncodedUtils.format(params, "utf-8");
+  
+ 		// Send post request
+ 		con.setDoOutput(true);
+ 		DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+ 		wr.writeBytes(urlParameters);
+ 		wr.flush();
+ 		wr.close();
+  
+ 		int responseCode = con.getResponseCode();
+
+ 		BufferedReader in = new BufferedReader(
+ 		        new InputStreamReader(con.getInputStream(),"iso-8859-1"),8);
+ 		String inputLine;
+ 		StringBuffer response = new StringBuffer();
+  
+ 		while ((inputLine = in.readLine()) != null) {
+ 			response.append(inputLine + "\n");
+ 		}
+ 		in.close();
+  
+ 		//print result
+ 		return response.toString();
+  
+ 	}
 }
 
